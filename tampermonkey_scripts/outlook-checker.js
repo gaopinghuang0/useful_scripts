f2cb30b91df4cc6606867ea47df3be070af54bd7
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-24 17:53:13 -0400

    add new tampermonkey script for outlook-checker.js

---
+++ tampermonkey_scripts/outlook-checker.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         outlook-checker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Check the number of unread email and notify
+// @author       Gaoping Huang
+// @match        https://outlook.office.com/owa/?realm=purdue.edu*
+// @require      https://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+  $(document).ready(function() {
+
+    repeat_check_unread_email()
+
+  });
+
+  function repeat_check_unread_email() {
+    var $inbox = $('span[title="Inbox"]'),
+      $unread_div = $inbox.next('div'),
+      $count_span = $unread_div.find('span');
+
+    setInterval(function() {
+      var count = parseInt($count_span.text());
+      if (count > 0) {
+        alert('New email: ' + count)
+      }
+    }, 30000);
+  }
+
+})();
